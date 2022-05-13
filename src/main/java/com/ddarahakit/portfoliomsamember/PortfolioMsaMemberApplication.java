caed662a37b86fb5291d4ba5522f49b7b1222cd1
minji <ghdalswl77@naver.com>
2022-05-13 14:15:58 +0900

    프로젝트 시작

---
+++ src/main/java/com/ddarahakit/portfoliomsamember/PortfolioMsaMemberApplication.java
@@ -0,0 +1,13 @@
+package com.ddarahakit.portfoliomsamember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioMsaMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioMsaMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddarahakit/portfoliomsamember/PortfolioMsaMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddarahakit.portfoliomsamember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioMsaMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
